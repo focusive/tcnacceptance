8548ce3fd9f794d6fb744d8ca818e9350b65bc20
Pallat Anchaleechamaikorn <pallat.anchaleechamaikorn@ktb.co.th>
2020-09-08 11:15:40 +0700

    done for check-in then check-out

--- cypress/integration/checkin.feature
+++ cypress/integration/checkin.feature
@@ -1,6 +1,9 @@
-Feature: Check-in
+Feature: Check-in then Check-out
 
     Scenario: Check-in To KhaoYai
         Given Open ThaiChana App At KhaoYai
         When Press Check-in Button
-        Then Show KhaoYai In The List
+        Then Show เช็คอินแล้ว
+
+    Scenario: Check-out
+        Then Press Check-out Button
--- cypress/support/step_definitions/checkin_spec.js
+++ cypress/support/step_definitions/checkin_spec.js
@@ -5,9 +5,18 @@ Given("Open ThaiChana App At KhaoYai", () => {
 });
 
 When('Press Check-in Button', keyword => {
-    cy.get('.btn').click()
+    // cy.get('button[id="checkin"]').invoke('click')
+    cy.wait(1000).contains("เช็คอินร้าน").click({ force: true })
+    cy.contains("โปรดระบุเบอร์โทรศัพท์")
+    cy.get('input').type('0812345678')
+    cy.contains("ยืนยัน").click({ force: true })
+    cy.contains("กำลังดำเนินการ")
 })
 
-Then('Show KhaoYai In The List', expected => {
-    cy.contains("KhaoYai")
+Then('Show เช็คอินแล้ว', expected => {
+    cy.contains("เช็คเอาท์")
+});
+
+Then('Press Check-out Button', expected => {
+    cy.contains("เช็คเอาท์").click({ force: true }).end()
 });
